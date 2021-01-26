8d70f7b2044481be30aa0e4079ab2cb902e63577
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-01-26 09:20:41 +0000

    Feat: Post Melon Order
    Feat: Error Message CSS Red

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -46,6 +46,19 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    let formData = {"qty": $("#qty-field").val(),
+                    "melon_type": $("#melon-type-field").val()};
+
+    $.post('/order-melons.json', formData, (res) => {
+      if (res.code === 'ERROR'){
+        $('#order-status').addClass("order-error");
+      }
+      else {
+        $('#order-status').removeClass("order-error");
+      }
+      $('#order-status').html(`${res.code}, ${res.msg}`);
+    });
+
 }
 
 $("#order-form").on('submit', orderMelons);
